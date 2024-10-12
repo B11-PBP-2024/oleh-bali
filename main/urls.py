eddb686ddbc3e99446633019f067acbde3c9d907
Ezar Surahman <akhdanezar@gmail.com>
2024-10-12 12:05:35 +0700

    add some auth functionality

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, login_buyer
 app_name = 'main'
 
 urlpatterns = [
-    path('',show_main,name='show_main')
+    path('',show_main,name='show_main'),
+    path('login/buyer',login_buyer,name="login_buyer")
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login,authenticate
+from django.contrib import messages
 
 # Create your views here.
-@login_required
-def show_main(request):
 
-    return render(request,'homepage.html')
+def show_main(request):
+    user = request.user
+    if user.is_seller():
+        return render(request,'homepage_seller.html')
+    else:
+        return render(request,'homepage_buyer.html')
+
+def login_buyer(request):
+    if request.method == "POST":
+        
+        form = AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            print(user)
+            if user.is_seller():
+                messages.error(request,"User is not registered as Buyer!")
+            else:
+                login(request,user)
+                return redirect('show_main')
+        else:
+            messages.error(request,"Invalid username or password!")
+    else:
+        form = AuthenticationForm()
+    
+    context = {
+        'form' : form,
+    }
+    return render(request,"login/login.html",context)

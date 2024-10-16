e52ff8667bc2e8a847d6d6aef0e2b612383b128c
Ezar Surahman <akhdanezar@gmail.com>
2024-10-16 14:06:01 +0700

    Add edit and delete article, and add styling and authorization middleware

--- article/urls.py
+++ article/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from article.views import show_articles, create_article, json_all_article, json_id_article
+from article.views import show_articles, create_article, json_all_article, json_id_article, delete_article,edit_article
 app_name = 'article'
 
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     path("create/",create_article, name="create_article"),
     path("json-all/",json_all_article, name="json_all_article"),
     path("json-id/<uuid:id>",json_id_article, name="json_id_article"),
+    path("delete/<uuid:id>",delete_article, name="delete_article"),
+    path("edit/<uuid:id>",edit_article, name="edit_article"),
 ]
--- oleh_bali/settings.py
+++ oleh_bali/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'article.middleware.BuyerRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'oleh_bali.urls'

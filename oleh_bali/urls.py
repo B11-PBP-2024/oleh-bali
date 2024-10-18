94ff603ffa6216ecf7ab53ac90ae0d5c1c8a5f42
yemimaclra <yemimanainggolan0@gmail.com>
2024-10-18 15:50:11 +0700

    Add products model and products form

--- oleh_bali/urls.py
+++ oleh_bali/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+
 #
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("",include("main.urls")),
     path("article/",include("article.urls")),
+    path("products/",include("seller.urls")),
 ]

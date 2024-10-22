078145212f5c937d372d67455d4bd260c880fc4f
rgeraldow <rgeraldow@gmail.com>
2024-10-23 01:18:14 +0700

    progress views

--- review/models.py
+++ review/models.py
@@ -1,6 +1,11 @@
 from django.db import models
-from main.models import Buyer
+from user_profile.models import BuyerProfile
+from seller.models import ProductEntry
+from django.utils import timezone
 
 class ReviewEntry(models.Model):
-    user = models.ForeignKey(Buyer,on_delete=models.CASCADE)
-    product = models.
+    user = models.ForeignKey(BuyerProfile,on_delete=models.CASCADE)
+    product = models.ForeignKey(ProductEntry,on_delete=models.CASCADE)
+    time = models.DateField(default=timezone.now)
+    review_text = models.TextField()
+
--- review/views.py
+++ review/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from review.models import ReviewEntry
+from seller.models import ProductEntry
 
 # Create your views here
 
-def show_review(request):
-    return render(request, "review_page.html")
+def show_review(request,id):
+    product = ProductEntry.objects.get(pk=id)
+    reviews = ReviewEntry.objects.filter(product=product)
+    context= {
+        'reviews':reviews,
+        'product':product
+    }
+    return render(request, "review_page.html", context)

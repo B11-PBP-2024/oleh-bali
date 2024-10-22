f642d510db7397c35ac48242248e8150691e5601
rgeraldow <rgeraldow@gmail.com>
2024-10-23 02:16:47 +0700

    progress review_page

--- review/models.py
+++ review/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from user_profile.models import BuyerProfile
+from user_profile.models import Buyer
 from seller.models import ProductEntry
 from django.utils import timezone
 
 class ReviewEntry(models.Model):
-    user = models.ForeignKey(BuyerProfile,on_delete=models.CASCADE)
+    user = models.ForeignKey(Buyer,on_delete=models.CASCADE)
     product = models.ForeignKey(ProductEntry,on_delete=models.CASCADE)
-    time = models.DateField(default=timezone.now)
+    time = models.DateTimeField(default=timezone.now)
     review_text = models.TextField()
 
--- review/views.py
+++ review/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from review.models import ReviewEntry
 from seller.models import ProductEntry
+from review.forms import ReviewEntryForm
 
 # Create your views here
 
 def show_review(request,id):
     product = ProductEntry.objects.get(pk=id)
-    reviews = ReviewEntry.objects.filter(product=product)
+    reviews = ReviewEntry.objects.filter(product=product).select_related('user', 'user__buyerprofile')
     context= {
         'reviews':reviews,
         'product':product
     }
     return render(request, "review_page.html", context)
+
+def create_review(request,id):
+    form = ReviewEntryForm(request.POST or None)
+    if form.is_valid() and request.method == "POST":
+        review = form.save(commit=False)
+        review.user = request.user
+        product = ProductEntry.objects.get(pk=id)
+        review.product = product
+        review.review_text = review.review_text.replace("/r","/n")
+        review.save()
+        return redirect('review:show_review', id=product.id)
+    
+    context = {'form': form}
+    return render(request, "create_review.html", context)
+

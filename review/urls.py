1395c0e0b19f2841a53b29a3a260acdac00746b9
rgeraldow <rgeraldow@gmail.com>
2024-10-23 02:16:47 +0700

    progress review_page

--- review/urls.py
+++ review/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from review.views import show_review
+from review.views import show_review, create_review
 
 app_name = "review"
 
 urlpatterns = [
-    path('<uuid:id>', show_review, name='show_review')
+    path('<uuid:id>', show_review, name='show_review'),
+    path('create/<uuid:id>', create_review, name='create_review'),
+    
+    
 ]

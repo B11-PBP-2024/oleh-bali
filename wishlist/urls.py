e0e04b88eb14ca873d7a262ebaada304cdd8a7ac
alya rasheeda <alyarasheeda@alyas-MacBook-Air.local>
2024-10-22 21:54:43 +0700

    update models.py

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -5,4 +5,5 @@ app_name = 'wishlist'
 
 urlpatterns = [
     path('', views.show_wishlist, name='show_wishlist'),
+    path('wishlist/delete/<int:id>/', views.delete_wishlist_item, name='delete_wishlist_item'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
-from django.db.models import Sum
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Min, Max
+from user_profile.models import BuyerProfile
 from .models import WishlistItem
+from .models import WishlistItem
+from django.urls import reverse
 
 def show_wishlist(request):
-    # Fetch wishlist items for the logged-in user
-    wishlist_items = WishlistItem.objects.filter(user=request.user)
+    buyer_profile = BuyerProfile.objects.get(user=request.user)
+    wishlist_items = WishlistItem.objects.filter(user=buyer_profile)
 
-    # Calculate total min and max price
-    total_min = wishlist_items.aggregate(Sum('min_price'))['min_price__sum'] or 0
-    total_max = wishlist_items.aggregate(Sum('max_price'))['max_price__sum'] or 0
+    total_min = 0
+    total_max = 0
+    for item in wishlist_items:
+        item_min = item.products.aggregate(min_price=Min('productseller__price'))['min_price']
+        item_max = item.products.aggregate(max_price=Max('productseller__price'))['max_price']
+
+        total_min += item_min or 0
+        total_max += item_max or 0
 
     context = {
         'wishlist_items': wishlist_items,
@@ -16,3 +24,8 @@ def show_wishlist(request):
         'total_max': total_max,
     }
     return render(request, 'wishlist/wishlist.html', context)
+
+def delete_wishlist_item(request, id):
+    wishlist_item = get_object_or_404(WishlistItem, pk=id, user=request.user.buyerprofile)
+    wishlist_item.delete()
+    return redirect(reverse('wishlist:show_wishlist'))

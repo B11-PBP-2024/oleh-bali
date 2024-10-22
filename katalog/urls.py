eebf8d7c0b696a8f3871c5d36b659b1e5e439a37
Ezar Surahman <akhdanezar@gmail.com>
2024-10-23 01:16:58 +0700

    Add product details

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from katalog.views import show_catalog, get_product_by_id, product_details
+from katalog.views import show_catalog, get_product_by_id, product_details, get_products
 app_name = 'katalog'
 
 urlpatterns = [
     path("",show_catalog,name="show_catalog"),
     path("json/<uuid:id>",get_product_by_id,name="get_product_by_id"),
+    path("json",get_products,name="get_products"),
     path("<uuid:id>",product_details,name="product_details"),
 
 ]

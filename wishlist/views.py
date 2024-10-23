e4059e12fc0209ab7d351214aefa761943940d84
alya rasheeda <alyarasheeda@alyas-MacBook-Air.local>
2024-10-23 14:39:14 +0700

    hubungin wishlist dari catalog

--- wishlist/views.py
+++ wishlist/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Min, Max
 from user_profile.models import BuyerProfile
 from .models import WishlistItem
-from .models import WishlistItem
 from django.urls import reverse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from seller.models import ProductEntry
+from django.contrib.auth.decorators import login_required
 
 def show_wishlist(request):
     buyer_profile = BuyerProfile.objects.get(user=request.user)
@@ -29,3 +32,42 @@ def delete_wishlist_item(request, id):
     wishlist_item = get_object_or_404(WishlistItem, pk=id, user=request.user.buyerprofile)
     wishlist_item.delete()
     return redirect(reverse('wishlist:show_wishlist'))
+
+@csrf_exempt
+def add_to_wishlist(request):
+    if request.method == "POST":
+        product_id = request.POST.get('product_id')
+        if not product_id:
+            return JsonResponse({'success': False, 'error': 'No product ID provided'})
+
+        buyer_profile = get_object_or_404(BuyerProfile, user=request.user)
+        product = get_object_or_404(ProductEntry, pk=product_id)
+
+        wishlist_item, created = WishlistItem.objects.get_or_create(name=product.product_name)
+
+        wishlist_item.products.add(product)
+        wishlist_item.user.add(buyer_profile)
+
+        return JsonResponse({'success': True})
+
+    return JsonResponse({'success': False, 'error': 'Invalid request method'})
+
+@login_required
+@csrf_exempt
+def add_to_wishlist_from_details(request):
+    if request.method == "POST":
+        product_id = request.POST.get('product_id')
+        if not product_id:
+            return JsonResponse({'success': False, 'error': 'No product ID provided'})
+
+        buyer_profile = get_object_or_404(BuyerProfile, user=request.user)
+        product = get_object_or_404(ProductEntry, pk=product_id)
+
+        wishlist_item, created = WishlistItem.objects.get_or_create(name=product.product_name)
+
+        wishlist_item.products.add(product)
+        wishlist_item.user.add(buyer_profile)
+
+        return JsonResponse({'success': True})
+
+    return JsonResponse({'success': False, 'error': 'Invalid request method'})

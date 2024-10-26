775375cf3c8ed11b94361e1d0e5f26d6109294ab
Ezar Surahman <akhdanezar@gmail.com>
2024-10-26 14:48:01 +0700

    Integrate catalog with wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('wishlist/delete/<int:id>/', views.delete_wishlist_item, name='delete_wishlist_item'),
     path('add/', views.add_to_wishlist, name='add_to_wishlist'),
     path('add_from_details/', views.add_to_wishlist_from_details, name='add_from_details'),
-
 ]

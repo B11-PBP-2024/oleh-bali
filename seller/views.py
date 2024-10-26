21abf282bbea3be99c38d71b0bdcae0a9c1bfcdb
yemimaclra <yemimanainggolan0@gmail.com>
2024-10-27 05:32:32 +0700

    Implementing AJAX on products seller page

--- seller/views.py
+++ seller/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from .models import ProductEntry, ProductSeller
 from .forms import ProductEntryForm, ProductSellerForm
 
@@ -42,31 +43,14 @@ def add_product_seller(request):
 
 @login_required
 def show_product_seller(request):
-    search_query = request.GET.get('search', '')      
-    category = request.GET.get('category')        
-    sort_price = request.GET.get('sort_price')  
 
     products_seller = ProductSeller.objects.filter(seller=request.user)
-
-    if search_query:
-        products_seller = products_seller.filter(Q(product__product_name__icontains=search_query))
-
-    if category:
-        products_seller = products_seller.filter(Q(product__product_category=category))
-
-    if sort_price == 'asc':
-        products_seller = products_seller.order_by('price')
-    elif sort_price == 'desc':
-        products_seller = products_seller.order_by('-price')
 
     categories = dict(ProductEntry._meta.get_field('product_category').choices) 
 
     return render(request, 'show_products_seller.html', {
         'products_seller': products_seller,
         'categories': categories,  
-        'search_query': search_query,
-        'category': category,
-        'sort_price': sort_price,
     })
 
 @login_required
@@ -86,3 +70,24 @@ def delete_product_seller(request, id):
     if request.method == 'POST':
         product.delete()
     return redirect('seller:show_product_seller')
+
+def product_search(request):
+    search_query = request.GET.get('search', '')      
+    category = request.GET.get('category')        
+    sort_price = request.GET.get('sort_price') 
+    products_seller = ProductSeller.objects.filter(seller=request.user).select_related('product').values(
+        "id", "price", "product__id", "product__product_name", "product__product_image"
+    )
+    if search_query:
+        products_seller = products_seller.filter(Q(product__product_name__icontains=search_query))
+
+    if category:
+        products_seller = products_seller.filter(Q(product__product_category=category))
+
+    if sort_price == 'asc':
+        products_seller = products_seller.order_by('price')
+    elif sort_price == 'desc':
+        products_seller = products_seller.order_by('-price')
+    return JsonResponse(list(products_seller), safe=False)
+
+

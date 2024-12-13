266ca4afe16f686ecf0e5384e5132fbaa37efffc
Ezar Surahman <akhdanezar@gmail.com>
2024-12-13 14:41:45 +0700

    add see stores json endpoint

--- see_stores/urls.py
+++ see_stores/urls.py
@@ -5,4 +5,5 @@ app_name = 'see_stores'
 
 urlpatterns = [
     path('<uuid:product_id>/stores/', views.see_stores, name='see_stores'),
+    path('<uuid:product_id>/stores/json/', views.get_stores_json, name='get_stores_json'),
 ]
--- see_stores/views.py
+++ see_stores/views.py
@@ -1,7 +1,11 @@
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from seller.models import ProductEntry, ProductSeller
 from user_profile.models import SellerProfile
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.core import serializers
 
 @login_required
 def see_stores(request, product_id):
@@ -27,3 +31,46 @@ def see_stores(request, product_id):
         'sellers_with_prices': sellers_with_prices,
     }
     return render(request, 'see_stores/see_stores.html', context)
+
+@csrf_exempt
+def get_stores_json(request, product_id):
+    product = get_object_or_404(ProductEntry, pk=product_id)
+    
+    sellers = SellerProfile.objects.filter(
+        user__productseller__product=product
+    ).distinct()
+
+    
+    sellers_with_prices = []
+    for seller in sellers:
+        seller_dict = {
+            'id': seller.user.id,
+            'store_name': seller.store_name,
+            'profile_picture': seller.profile_picture,
+            'city': seller.city,
+            'subdistrict': seller.subdistrict,
+            'village' : seller.village,
+            'address': seller.address,
+            'maps': seller.maps,
+        }
+
+        try:
+            price = ProductSeller.objects.filter(seller=seller.user, product=product)
+        except ProductSeller.DoesNotExist:
+            price = "N/A"
+        for every_price in price:
+            sellers_with_prices.append({
+                'seller': seller_dict,
+                'price': every_price.price
+            })
+    data = {
+        'product': {
+            'id': product.id,
+            'product_name': product.product_name,
+            'product_category': product.product_category,
+            'description': product.description,
+            'product_image': product.product_image,
+        },
+        'sellers_with_prices': sellers_with_prices
+    }
+    return JsonResponse(data, safe=False)
